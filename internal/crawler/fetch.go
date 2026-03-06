2d14f5f5c10b30b7e141b643b3e95eda9e4a2fb0
Elias-Larsson <aroezi05@gmail.com>
2026-03-06 14:04:55 +0100

    add slice for extractedlinks

--- internal/crawler/fetch.go
+++ internal/crawler/fetch.go
@@ -12,26 +12,35 @@ func extractHtmlPage(url string) {
 	if err != nil {
 		panic(err)
 	}
-
 	defer resp.Body.Close()
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	extractLinks(doc)
 
+	links := extractLinks(doc)
+	fmt.Println(links)
 }
 
-func extractLinks(n *html.Node) {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, attr := range n.Attr {
-			if attr.Key == "href" {
-				fmt.Println(attr.Val)
+func extractLinks(n *html.Node) []string {
+	var links []string
+
+	var walk func(*html.Node)
+	walk = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, attr := range node.Attr {
+				if attr.Key == "href" {
+					links = append(links, attr.Val)
+				}
 			}
 		}
+
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			walk(c)
+		}
 	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		extractLinks(c)
-	}
+
+	walk(n)
+	return links
 }
